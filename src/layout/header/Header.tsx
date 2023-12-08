aaa1c06683adb5dc31dec283118153908ed5f61e
Alexander Korobov <dktwho@mail.ru>
2023-12-08 16:41:45 +0300

    landing: create component Menu

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
+import {Menu} from "../../components/menu/Menu";
 
 export const Header = () => {
     return (
         <StyledHeader>
             <Logo/>
-
-            <nav>
-                <ul>
-                    <li>
-                        <a href="">Home</a>
-                    </li>
-                    <li>
-                        <a href="">Skills</a>
-                    </li>
-                    <li>
-                        <a href="">Works</a>
-                    </li>
-                    <li>
-                        <a href="">Testimony</a>
-                    </li>
-                    <li>
-                        <a href="">Contacts</a>
-                    </li>
-                </ul>
-            </nav>
+            <Menu/>
         </StyledHeader>
     );
 };
