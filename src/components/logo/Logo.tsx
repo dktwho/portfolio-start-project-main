904b62ba77cecce63311fa5cb8588a01a286d2ed
Alexander Korobov <dktwho@mail.ru>
2023-12-08 17:41:43 +0300

    landing: added viewBox props Icon

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,7 +4,7 @@ import {Icon} from "../icon/Icon";
 export const Logo = () => {
     return (
         <a href="">
-            <Icon iconId={'code'} width={'30'} height={'30'}/>
+            <Icon iconId={'code'} width={'50'} height={'50'} viewBox={'0 0 50 50'}/>
         </a>
     );
 };
