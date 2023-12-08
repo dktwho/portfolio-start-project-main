f42101b377a9678e0c1f640991cc59fb410a56a1
Alexander Korobov <dktwho@mail.ru>
2023-12-08 16:35:06 +0300

    landing: landing structure, Header with nav

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header>
+            <a href="">
+                <img src="" alt=""/>
+            </a>
+
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Skills</a>
+                    </li>
+                    <li>
+                        <a href="">Works</a>
+                    </li>
+                    <li>
+                        <a href="">Testimony</a>
+                    </li>
+                    <li>
+                        <a href="">Contacts</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
