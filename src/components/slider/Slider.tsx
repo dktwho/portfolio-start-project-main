f39b7d945d509f9e3a427bc5f9861d231fc14951
Alexander Korobov <dktwho@mail.ru>
2023-12-11 17:35:44 +0300

    feat:add testimony to App, styled StyledTestimony

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -4,11 +4,27 @@ import styled from "styled-components";
 export const Slider = () => {
     return (
         <StyledSlider>
-
+            <Slide>
+                <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</Text>
+                <Name>@Ivan Ivanov</Name>
+            </Slide>
         </StyledSlider>
     );
 };
 
 const StyledSlider = styled.div`
+border: 1px solid red
+`
+
+const Slide = styled.div`
 
 `
+
+const Text = styled.div`
+
+`
+
+const Name = styled.div`
+
+`
+
