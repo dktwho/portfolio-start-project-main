f100a7bbc0de2c37067d85fcc3fc71aa83e11cab
Alexander Korobov <dktwho@mail.ru>
2023-12-11 17:31:45 +0300

    feat:prepare new block testimony, create component Slider, and Testimony

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from "styled-components";
+
+export const Slider = () => {
+    return (
+        <StyledSlider>
+
+        </StyledSlider>
+    );
+};
+
+const StyledSlider = styled.div`
+
+`
