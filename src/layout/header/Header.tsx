a072d7c8916512bc51b72093dc037ba9654eec01
Alexander Korobov <dktwho@mail.ru>
2023-12-21 18:53:23 +0300

    style:used Container and FlexWrapper for Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,21 +2,24 @@ import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
 
-const items = ['Home', 'Skills', 'Works', 'Testimony', 'Contacts' ]
+const items = ['Home', 'Skills', 'Works', 'Testimony', 'Contacts']
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items} />
+            <Container>
+                <FlexWrapper justify={'space-between'} align={'center'}>
+                    <Logo/>
+                    <Menu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
   background-color: #76dc6e;
-  display: flex;
-  justify-content: space-between;
-  
 `
 
