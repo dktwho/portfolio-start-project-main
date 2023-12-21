90fbfe2235026dcee06521957b0aa423ac6749f0
Alexander Korobov <dktwho@mail.ru>
2023-12-21 19:12:49 +0300

    style:create HeaderMenu, and styled

---
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from "styled-components";
+import {theme} from "../../../styles/Theme";
+
+export const HeaderMenu = (props: { menuItems: Array<string> }) => {
+    const {menuItems} = props
+    return (
+        <StyledHeaderMenu>
+            <ul>
+                {menuItems.map((item, index) => {
+                    return (
+                        <ListItem key={index}>
+                            <Link href="">{item}
+                            <Mask><span>{item}</span></Mask>
+                            <Mask><span>{item}</span></Mask>
+                            </Link>
+                        </ListItem>
+                    )
+                })}
+            </ul>
+        </StyledHeaderMenu>
+    );
+};
+
+
+const StyledHeaderMenu = styled.nav`
+  ul {
+    display: flex;
+    gap: 30px;
+    justify-content: center;
+  }
+`
+
+const ListItem = styled.li`
+    position: relative;
+
+`
+
+const Link = styled.a`
+  text-align: center;
+  font-family: Josefin Sans, sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+
+`
+
+const Mask = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: inline-block;
+  height: 50%;
+  overflow: hidden;
+  outline: 1px solid red;
+  color: ${theme.colors.accent}
+`
