064a9c381489ed2159f23ed82738aa0ad4b533b1
Alexander Korobov <dktwho@mail.ru>
2023-12-22 17:44:34 +0300

    style:styled main block

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import styled from "styled-components";
 import photo from '../../../assets/images/JP_02959.jpg'
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={'center'} justify={'space-around'} >
-                <div>
-                    <span>Hi There</span>
-                    <NameTitle>I am Alex Korobov</NameTitle>
-                    <MainTitle>A Frontend Developer</MainTitle>
-                </div>
-                <Photo src={photo}/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper align={'center'} justify={'space-between'}>
+                    <div>
+                        <SmallText>Hi There</SmallText>
+                        <NameTitle>I am <span>Alex Korobov</span></NameTitle>
+                        <MainTitle>A Frontend Developer</MainTitle>
+                    </div>
+                    <Photo src={photo}/>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
@@ -27,13 +31,40 @@ const Photo = styled.img`
 const StyledMain = styled.div`
   min-height: 100vh;
   background-color: #9a9795;
+  display: flex;
 `
 const MainTitle = styled.h1`
-  font-size: 36px;
+  font-size: 27px;
+  font-weight: 400;
 `
 
 const NameTitle = styled.h2`
-  font-size: 16px;
+  font-size: 50px;
+  font-family: 'Josefin Sans', sans-serif;
+  letter-spacing: 0.05em;
+  font-weight: 700;
+  margin: 10px 0;
+  
+  span {
+    position: relative;
+    z-index: 0;
+    &::before {
+      content: '';
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+      
+    }
+  }
+`
+
+const SmallText = styled.h2`
+  font-weight: 400;
+  font-size: 14px;
 `
 
 
