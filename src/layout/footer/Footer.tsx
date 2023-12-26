6ef48df24ca175fb3fdd4a44466058d83701fbab
Alexander Korobov <dktwho@mail.ru>
2023-12-26 17:07:07 +0300

    style:styled block Footer, change sprite color currentColor.

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 export const Footer = () => {
     return (
@@ -29,29 +30,52 @@ export const Footer = () => {
 
 
 const StyledFooter = styled.footer`
-  background-color: #74f16e;
-  min-height: 20vh;
+  background-color: ${theme.colors.primaryBg};
+  padding: 40px 0;
 `
 
 const Name = styled.span`
-
+  font-family: Josefin Sans, sans-serif;
+  font-size: 22px;
+  font-weight: 700;
+  letter-spacing: 3px;
 `
 
 const SocialList = styled.ul`
   display: flex;
-  gap: 30px;
+  gap: 20px;
+  margin: 30px 0;
 `
 
 const SocialItem = styled.li`
-  
+
 `
 
 const SocialLink = styled.a`
-
+  background-color: rgba(255, 255, 255, 0.10);
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  color: ${theme.colors.accent};
+  
+  &:hover {
+    color: ${theme.colors.primaryBg};
+    background-color: ${theme.colors.accent};
+    transform: translateY(-6px);
+  }
+  
 `
 
 const Copyright = styled.small`
-
+  text-align: center;
+  font-size: 12px;
+  font-weight: 400;
+  opacity: 0.3;
 `
 
 
