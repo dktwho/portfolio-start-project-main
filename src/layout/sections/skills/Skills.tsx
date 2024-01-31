80d2699cf7de64fdfa405d3c5e8422309d03242e
Alexander Korobov <dktwho@mail.ru>
2024-01-31 09:32:11 +0300

    style:apply media for Skill

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -11,12 +11,18 @@ export const Skills = () => {
             <Container>
                 <SectionTitle>My skills</SectionTitle>
                 <FlexWrapper wrap={'wrap'} justify={'space-between'}>
-                    <Skill iconId={'code'} title={'HTML5'} text={'There are many variations of passages of Lorem Ipsum '}/>
-                    <Skill iconId={'css'} title={'CSS3'} text={'Lorem Ipsum has been the industry\'s standard dummy text .'}/>
-                    <Skill iconId={'react'} title={'REACT'} text={'Content here, content here\', making it look like readable English.'}/>
-                    <Skill iconId={'typescript'} title={'Typescript'} text={'There are many variations of passages of Lorem Ipsum '}/>
-                    <Skill iconId={'styled'} title={'styled'} text={'Lorem Ipsum has been the industry\'s standard dummy text .'}/>
-                    <Skill iconId={'figma'} title={'figma'} text={'Content here, content here\', making it look like readable English.'}/>
+                    <Skill iconId={'code'} title={'HTML5'}
+                           text={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,'}/>
+                    <Skill iconId={'css'} title={'CSS3'}
+                           text={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,'}/>
+                    <Skill iconId={'react'} title={'REACT'}
+                           text={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,'}/>
+                    <Skill iconId={'typescript'} title={'Typescript'}
+                           text={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,'}/>
+                    <Skill iconId={'styled'} title={'styled'}
+                           text={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,'}/>
+                    <Skill iconId={'figma'} title={'figma'}
+                           text={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,'}/>
                 </FlexWrapper>
             </Container>
         </StyledSkills>
