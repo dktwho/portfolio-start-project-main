cc788f5f4b6917a8da12c248ed770b9b8d55f3f3
Alexander Korobov <dktwho@mail.ru>
2024-02-06 12:20:51 +0300

    feat:create Main_styles

---
+++ src/layout/sections/main/Main_Styles.ts
@@ -0,0 +1,95 @@
+import {theme} from "../../../styles/Theme";
+import styled from "styled-components";
+import {font} from "../../../styles/Common";
+
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 4;
+
+
+  &::before {
+    content: '';
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${theme.colors.accent};
+
+    position: absolute;
+    top: -24px;
+    left: 24px;
+    z-index: -1;
+
+    @media ${theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+      top: -17px;
+      left: 20px;
+    }
+  }
+
+  @media ${theme.media.mobile} {
+    margin-top: 65px;
+  }
+`
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+  margin-right: 20px;
+
+  @media ${theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+  }
+`
+
+const Main = styled.section`
+  min-height: 100vh;
+  background-color: ${theme.colors.secondaryBg};
+  display: flex;
+`
+const MainTitle = styled.h1`
+  ${font({weight: 400, Fmax: 27, Fmin: 20})}
+`
+
+const NameTitle = styled.h2`
+  ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 50, Fmin: 36})}
+  letter-spacing: 0.05em;
+  margin: 10px 0;
+
+  span {
+    position: relative;
+    z-index: 0;
+    white-space: nowrap;
+
+    &::before {
+      content: '';
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+    }
+  }
+
+  @media ${theme.media.mobile} {
+    margin: 15px 0 20px;
+
+  }
+`
+
+const SmallText = styled.h2`
+  font-weight: 400;
+  font-size: 14px;
+`
+
+export const S = {
+    PhotoWrapper,
+    Photo,
+    Main,
+    MainTitle,
+    NameTitle,
+    SmallText
+}
