4beb23d31e665c9ba0640f0b9219f2474a7814bd
Alexander Korobov <dktwho@mail.ru>
2024-02-06 12:33:04 +0300

    feat:create Skill_styles with common styles for Skill

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Icon} from "../../../../components/icon/Icon";
-import styled from "styled-components";
 import {FlexWrapper} from "../../../../components/FlexWrapper";
-import {theme} from "../../../../styles/Theme";
+import {S} from './Skill_Styles'
 
 
 type SkillPropsType = {
@@ -13,52 +12,16 @@ type SkillPropsType = {
 export const Skill = (props: SkillPropsType) => {
     const {iconId, title, text} = props
     return (
-        <StyledSkill>
+        <S.Skill>
             <FlexWrapper direction={'column'} align={'center'}>
-                <IconWrapper>
+                <S.IconWrapper>
                     <Icon iconId={iconId}/>
-                </IconWrapper>
-                <SkillTitle>{title}</SkillTitle>
-                <SkillText>{text}</SkillText>
+                </S.IconWrapper>
+                <S.SkillTitle>{title}</S.SkillTitle>
+                <S.SkillText>{text}</S.SkillText>
             </FlexWrapper>
-        </StyledSkill>
+        </S.Skill>
     );
 };
 
 
-const StyledSkill = styled.div`
-  width: 330px;
-  flex-grow: 1;
-  padding: 42px 20px 52px;
-  
-  @media ${theme.media.mobile} {
-    padding: 62px 0 40px;
-  }
-`
-
-const SkillTitle = styled.h3`
-  margin: 70px 0 15px;
-  text-transform: uppercase;
-`
-
-const SkillText = styled.p`
-  text-align: center;
-`
-
-export const IconWrapper = styled.div`
-  position: relative;
-
-  &::before {
-    content: '';
-    display: inline-block;
-    width: 80px;
-    height: 80px;
-    background: rgba(255, 255, 255, 0.1);
-    transform: rotate(45deg) translate(-50%, -50%);
-
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform-origin: top left;
-  }
-`
